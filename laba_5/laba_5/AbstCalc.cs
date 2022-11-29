82d921d22a6d1f00dd01f46e6832bb58168c2b02
Apach26 <119453334+Apach26@users.noreply.github.com>
2022-11-29 22:56:06 +0200

    Add files via upload

---
+++ laba_5/laba_5/AbstCalc.cs
@@ -0,0 +1,13 @@
+﻿
+public abstract class AbstCalc
+{
+    public string Model { get; protected set; }
+
+    public abstract double Add();
+
+    public abstract double Sub();
+
+    public abstract double Div();
+
+    public abstract double Mul();
+}
